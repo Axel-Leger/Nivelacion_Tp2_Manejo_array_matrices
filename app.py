4969b2e5d8e06bb4b792389558ab13d17848c922
Axel Leger <axelleger2@gmail.com>
2025-04-14 09:27:42 -0300

    creacion de carpetas y servidor con flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+personas = []
+
+@app.route("/agregar", methods=["POST"])
+def agregar():
+    data = request.get_json()
+    nombre = data.get("nombre")
+    edad = int(data.get("edad"))
+    nota = float(data.get("nota"))
+    
+    personas.append([nombre, edad, nota])
+
+    return "", 200
+
+
+@app.route("/mostrar", methods = ["GET"])
+def mostrar():
+    return jsonify({"original": personas, "ordenado": sorted(personas, key=lambda x: x[2], reverse=True)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
